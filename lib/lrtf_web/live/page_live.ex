30d84c3c637b99c2094a6ee85790d4ed643d635b
Geoffrey Lessel <code@geoffreylessel.com>
2022-10-08 11:38:41 -0500

    feat: subscribe to Lrtf on mount

--- lib/lrtf_web/live/page_live.ex
+++ lib/lrtf_web/live/page_live.ex
@@ -2,6 +2,8 @@ defmodule LrtfWeb.PageLive do
   use LrtfWeb, :live_view
 
   def mount(_params, _session, socket) do
+    if connected?(socket), do: Lrtf.subscribe(self())
+
     companies = Lrtf.list_companies()
     {:ok, assign(socket, companies: companies)}
   end
