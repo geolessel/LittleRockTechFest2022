1237767669a3ad81a667980cd886a1483012fc72
Geoffrey Lessel <code@geoffreylessel.com>
2022-10-08 11:48:42 -0500

    feat: add comment form

--- lib/lrtf_web/live/page_live.ex
+++ lib/lrtf_web/live/page_live.ex
@@ -5,7 +5,8 @@ defmodule LrtfWeb.PageLive do
     if connected?(socket), do: Lrtf.subscribe(self())
 
     companies = Lrtf.list_companies()
-    {:ok, assign(socket, companies: companies)}
+    comments = Lrtf.list_comments()
+    {:ok, assign(socket, comments: comments, companies: companies)}
   end
 
   def handle_info({:new_prices, companies}, socket) do
@@ -38,6 +39,26 @@ defmodule LrtfWeb.PageLive do
             <% end %>
           </tbody>
         </table>
+      </div>
+      <.comment_form comments={@comments} />
+    </div>
+    """
+  end
+
+  def comment_form(assigns) do
+    ~H"""
+    <div class="comments">
+      <%= f = form_for(:comments, "#", [phx_submit: :submit_comment]) %>
+
+      <div class="comment-form">
+        <h3>Comments</h3>
+        <%= text_input(f, :text) %>
+      </div>
+
+      <div id="comments-container">
+        <%= for comment <- @comments do %>
+          <div class="comment" id={"comment_#{comment.id}"}><%= comment.text %></div>
+        <% end %>
       </div>
     </div>
     """
